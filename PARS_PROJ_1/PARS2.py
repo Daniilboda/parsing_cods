be364fad7d5f15c9ed1719fc3fbe1ad9d84b4abf
Daniilboda <bodukhind@mail.ru>
2025-11-06 02:02:10 +0300

    ТРЕТИЙ

--- PARS_PROJ_1/PARS2.py
+++ PARS_PROJ_1/PARS2.py
@@ -1,9 +1,10 @@
 import json
-
 from bs4 import BeautifulSoup
 import lxml
 import requests
-#
+import csv
+from time import sleep
+import random
 # url = 'https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
 #
 headers = {
@@ -37,12 +38,83 @@ headers = {
 
 with open('all_cat_dict.json', 'r', encoding='utf-8') as file:
     all_categories = json.load(file)
+iter_count = int(len(all_categories)) - 1
+print(f'Всего иттераци: {iter_count}')
+counter = 0
 
 for cat_name, cat_href in all_categories.items():
-    rep = [' ', ',', '-']
-    for el in rep:
-        if el in cat_name:
-            cat_name = cat_name.replace(el, '_')
-    print(cat_name)
-    req = requests.get(url=cat_href, headers=headers)
-    src = req.text
+    if counter == 0:
+        rep = [' ', ',', '-']
+        for el in rep:
+            if el in cat_name:
+                cat_name = cat_name.replace(el, '_')
+
+        req = requests.get(url=cat_href, headers=headers)
+        src = req.text
+        with open(f'data/{counter}_{cat_name}.html', 'w', encoding='utf-8') as file_w:
+            print(src, file=file_w)
+        with open(f'data/{counter}_{cat_name}.html', 'r', encoding='utf-8') as file:
+            src = file.read()
+        soup = BeautifulSoup(src, 'lxml')
+
+        alert_block = soup.find(class_='uk-alert-danger')
+        if alert_block is not None:
+            continue
+        table_head = soup.find(class_='uk-overflow-container').find('tr').find_all('th')
+        product = table_head[0].text
+        kkal = table_head[1].text
+        protein = table_head[2].text
+        fat = table_head[3].text
+        carbs = table_head[4].text
+        with open(f'data/{counter}_{cat_name}.csv', 'w', encoding='windows-1251', newline='') as file1:
+            writer = csv.writer(file1, delimiter=';')
+            writer.writerow(
+                (
+                    product,
+                    kkal,
+                    protein,
+                    fat,
+                    carbs
+                )
+            )
+        product_data = soup.find(class_='uk-overflow-container').find('tbody').find_all('tr')
+
+        product_info = []
+        for item in product_data:
+            product_tds = item.find_all('td')
+            product = product_tds[0].find('a').text
+            kkal = product_tds[1].text
+            protein = product_tds[2].text
+            fat = product_tds[3].text
+            carbs = product_tds[4].text
+
+            product_info.append({
+                'Product': product,
+                'Kkal': kkal,
+                'Protein': protein,
+                'Fat': fat,
+                'Carbs': carbs
+            })
+
+            with open(f'data/{counter}_{cat_name}.csv', 'a', encoding='windows-1251', newline='') as file1:
+                writer = csv.writer(file1, delimiter=';')
+                writer.writerow(
+                    (
+                        product,
+                        kkal,
+                        protein,
+                        fat,
+                        carbs
+                    )
+                )
+        with open(f'data/{counter}_{cat_name}.json', 'a', encoding='utf-8') as file2:
+            json.dump(product_info, file2, indent=4, ensure_ascii=False)
+        counter += 1
+        print(f'|Итерация {counter}: {cat_name} записан...|')
+        iter_count -= 1
+        if iter_count == 0:
+            print('ГОТОВО!')
+            break
+        print(f'Осталось иттераций: {iter_count}')
+        sleep(random.randrange(1, 3))
+
